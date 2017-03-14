ca15dc052443d4bd151dc4476693fa69113548e3
Bule <586921@gmail.com>
2017-03-14 11:55:56 +0800

    Update VibratorHW.java
    Vibration add level file node， In the kernel has been supported 
    Details are here >> https://github.com/lbule/android_kernel_leeco_MT6795/commit/a999f9fdce0d419d5c20f4d82a9fa890a0ebe3a6

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -18,62 +18,35 @@ package org.cyanogenmod.hardware;
 
 import org.cyanogenmod.internal.util.FileUtils;
 
-/* 
- * Vibrator intensity adjustment
- *
- * Exports methods to get the valid value boundaries, the
- * default and current intensities, and a method to set
- * the vibrator.
- *
- * Values exported by min/max can be the direct values required
- * by the hardware, or a local (to VibratorHW) abstraction that's
- * internally converted to something else prior to actual use. The
- * Settings user interface will normalize these into a 0-100 (percentage)
- * scale before showing them to the user, but all values passed to/from
- * the client (Settings) are in this class' scale.
- */
-
-/* This would be just "Vibrator", but it conflicts with android.os.Vibrator */
 public class VibratorHW {
 
-    // Keep this synced to immvibe impl
-    private static final String INTENSITY_FILE = "/data/.libimmvibeclient_force";
+    private static String LEVEL_PATH = "/sys/class/timed_output/vibrator/level";
 
     public static boolean isSupported() {
         return true;
     }
 
-    public static boolean setIntensity(int intensity)  {
-        return FileUtils.writeLine(INTENSITY_FILE, Integer.toString(intensity));
-    }
-
     public static int getMaxIntensity()  {
-        return 127;
+        return 3300;
     }
 
     public static int getMinIntensity()  {
-        return 3;
+        return 1500;
     }
 
     public static int getWarningThreshold()  {
-        // actually this is rather arbitrary
-        return 115;
+        return 3200;
     }
 
     public static int getCurIntensity()  {
-        final String result = FileUtils.readOneLine(INTENSITY_FILE);
-        if (result == null) {
-            return 96;
-        }
-
-        try {
-            return Integer.parseInt(result.trim());
-        } catch (final NumberFormatException ignored) {
-            return 96;
-        }
+        return Integer.parseInt(FileUtils.readOneLine(LEVEL_PATH));
     }
 
     public static int getDefaultIntensity()  {
-        return 96;
+        return 3000;
+    }
+http://blog.csdn.net/benjaminwan/article/details/43901317
+    public static boolean setIntensity(int intensity)  {
+        return FileUtils.writeLine(LEVEL_PATH, String.valueOf(intensity));
     }
 }
