c068e44bfa7f8c1066c0afb302caf8df09e9f108
Unknown <19wisnia89@gmail.com>
2017-10-13 09:27:34 +0200

    gps: add gps.conf, no more warning gps.conf not found

--- device.mk
+++ device.mk
@@ -249,6 +249,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/slp_conf:system/etc/slp_conf \
+	$(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \ 
     $(LOCAL_PATH)/configs/clatd.conf:system/etc/clatd.conf \
     $(LOCAL_PATH)/configs/custom.conf:system/etc/custom.conf \
     $(LOCAL_PATH)/configs/mtklog-config.prop:system/etc/mtklog-config.prop \
