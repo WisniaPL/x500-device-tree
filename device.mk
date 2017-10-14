bd5ca3c0ca1a466a982e095f80fdb9e0a2e9bf5a
Unknown <19wisnia89@gmail.com>
2017-10-14 18:38:39 +0200

    Add HAL implementation for Oreo

--- device.mk
+++ device.mk
@@ -93,6 +93,9 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
+android.hardware.wifi@1.0-service \
+    wificond \
+    wifilogd \
     lib_driver_cmd_mt66xx \
     libwpa_client \
     wpa_supplicant
@@ -295,6 +298,7 @@ PRODUCT_PACKAGES += \
     
 # GPS
 PRODUCT_PACKAGES += \
+ android.hardware.gnss@1.0-impl \
     gps.mt6795 \
     libcurl
 
@@ -311,3 +315,61 @@ PRODUCT_PACKAGES += \
     libm4u  \
     libbwc  \
 ResurrectionOTA
+
+# Audio HAL
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@2.0-service
+#    android.hardware.soundtrigger@2.0-impl \
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
+
+# Camera HAL
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+android.hardware.camera.provider@2.4-service
+
+# Graphic HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+android.hardware.graphics.mapper@2.0-impl
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
+
+# Memtrack HAL
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack@1.0-impl
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    android.hardware.sensors@1.0-impl \
+android.hardware.sensors@1.0-service \
+
+#USB HAL
+PRODUCT_PACKAGES += \
+android.hardware.usb@1.0-service
+
+
+
